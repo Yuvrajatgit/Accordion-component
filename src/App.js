dfb89ac091a6595c9f0521b1bd0b885a3c01df69
Yuvraj Gupta <Yuvraj19992@gmail.com>
2022-03-31 22:50:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Accordion from './components/Accordion';
+import react from 'react';
+
+function App() {
+  return (
+  <Accordion/>
+  );
+}
+
+export default App;
---
+++ src/components/Data.js
@@ -0,0 +1,17 @@
+export const Data = [
+    {
+        question: 'What do you call an acid with an attitude?',
+        answer: 'A mean-o-acid!',
+        ID: 1
+    },
+    {
+        question: 'What do you call the wife of a hippie?',
+        answer: 'A Mississippi!',
+        ID: 2
+    },
+    {
+        question: "What do you call an elephant that doesn't matter?",
+        answer: 'An irrelephant!',
+        ID: 3
+    }
+];
